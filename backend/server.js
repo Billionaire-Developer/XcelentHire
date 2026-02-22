54e6c413e2aa73131b22618737cb10d17112a508
Billionaire_Developer <lawanijeremiah23@gmail.com>
2026-02-22 20:32:14 +0100

    xcelenthire

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const supabase = require("./supabase");
 const applicationRoutes = require("./routes/applicationRoutes");
 
 const app = express();
